bebbc1d8ec174cfa17b61d1522d22f7777b09ff7
a_liang <1291162027@qq.com>
2021-05-05 15:21:59 +0800

    test

--- test/ReadText.py
+++ test/ReadText.py
@@ -1,34 +1,37 @@
 import pandas as pd
 import numpy as np 
 from io import BytesIO
-df_1 = pd.read_excel("D:\浏览器下载\泰迪杯数据\附件1.xlsx" , header=0)
+import math 
+df_1 = pd.read_excel("D:\BaiduNetdiskDownload\hello\.vscode\数据\附件1.xlsx" , header=0)
 
 industry_table = []
 
+'''
+找到制造业股票代码
+'''
 for text in df_1.index :
     text=df_1.loc[text]
     if(text['所属行业'] == '制造业'):
         industry_table.append(int(text['股票代码']))
-
-df_2 = pd.read_csv("D:\浏览器下载\泰迪杯数据\\analyze.csv" , header=0 ,index_col = 0) 
+        
+'''
+找到所有制造业的数据  并初始化 
+'''
+df_2 = pd.read_csv("D:\BaiduNetdiskDownload\hello\.vscode\数据\附件2(样例数据).csv" , header=0 ,index_col = 0) 
 manufactureTable = []
 for i in df_2.index :
     if i in industry_table :
         manufactureTable.append(df_2.loc[i]) 
-for text   in manufactureTable :
-    print(text['END_DATE'])
-# list = [1 ,2,3,5,8]x
-# ad = np.array(list)
-# for i in ad : 
-#     print(i)
-
-
-# # encode str→bytes bytes→str decode
-# data = BytesIO("1 2 3\n 4 5 6".encode())
-# text = np.genfromtxt(data, names="A, B, C")
-# # 输出 A属性 列
-# print(text['A'])
-# # 输出第 1 行 
-# print(text[0])
-
-# np.array(shape ,ndim , dtype)
+for text in manufactureTable :
+    # print(text['END_DATE'])
+    for i in range(len(text)):
+        if type(text[i]) == type('A'):
+            continue
+        if math.isnan( float(text[i]) ) :
+            text[i] = 0
+with open ("data.csv" , 'w') as f :
+    for text   in manufactureTable :
+        for i in range(len(text)):
+            f.write( str(text[i]) ) 
+            f.write("  ")
+        f.write('\n')
